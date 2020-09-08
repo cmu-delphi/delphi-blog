293647adbb27462d79917c4ada7245455846b924
Alex Reinhart <alex@refsmmat.com>
2020-09-08 10:51:46 -0400

    Also fix the build by installing covidcast
    Not currently necessary, but maybe if we switch back to using the CI...

--- R/prepare.R
+++ R/prepare.R
@@ -1,3 +1,6 @@
-install.packages(c('blogdown', 'svglite'))
+install.packages(c('blogdown', 'svglite', 'devtools'))
 
 blogdown::install_hugo()
+
+devtools::install_github("cmu-delphi/covidcast", ref = "main",
+                         subdir = "R-packages/covidcast")
